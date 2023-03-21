c47790b639a4bb750e774e60d67da97ef213b66a
Gilbert Yim <justgilbertyim@gmail.com>
2023-03-20 16:10:38 -1000

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023WASNO",
+      "defaultValue": "2023wasno",
       "required": "true",
       "disabled": "true"
     },
