dd08d11a64c729590a01dba776e6f85305b93183
Gilbert Yim <justgilbertyim@gmail.com>
2023-03-20 16:23:51 -1000

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "1clkSWRZr460jrJQqn5DNfEAgO0ZxjmmIKRqK2ISRmkyNuGJVD8KmLfpgixnsgX6";
 /**
  * Get list of teams in event
  *
